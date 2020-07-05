22a117c820801d6858e32acd06ca06ffd9fb4907
dziljalal1 <dziljalal1@gmail.com>
2020-07-05 18:24:14 +0700

    Memulai belajar dari buku Bamabang Hariyanto, Esensi Java Revisi Ke 5
    Bab 4, mengakses properties java

---
+++ src/com/dzil/bambanghariyanto/bab4/SystemProperties.java
@@ -0,0 +1,8 @@
+package com.dzil.bambanghariyanto.bab4;
+
+public class SystemProperties {
+
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+    }
+}
